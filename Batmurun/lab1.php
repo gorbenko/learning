ddb0aa164678d13e5cadbd483a2baca4e464d3b7
Oleg Gorbenko <oggo@yandex-team.ru>
2014-11-12 15:23:20 +0500

    Batmurum Adya — Hometask #1

---
+++ Batmurun/lab1.php
@@ -0,0 +1,23 @@
+﻿<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<?php
+$myfile = fopen("c://lab1//lab1.txt", "r") or die("Unable to open file!");
+// Output one character until end-of-file
+$string ="";
+while(!feof($myfile)) {
+	$var = fgetc($myfile);
+	if($var == '#') 
+	{
+	 echo $string.'<br>';
+	 $string ="";
+	 $var="";
+	}	
+	$string = $string.$var[0];
+}
+fclose($myfile);
+?>
+<form action="writetxt.php" method=post>
+  Имя: <input type="text" name="input1"><br>
+  <textarea rows="4" cols="50" name="textarea1">
+  </textarea>
+  <input type="submit" value="Submit">
+</form>
